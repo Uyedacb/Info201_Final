6054c2cdebc0784c95679a164e06ade33ee75817
Uyedacb <uyedacb@uw.edu>
2018-05-29 20:11:56 -0700

    add spider plot descript

--- main_ui.R
+++ main_ui.R
@@ -32,7 +32,25 @@ main_ui <- fluidPage(
         # outputs spider plot and plot description
         mainPanel(
           chartJSRadarOutput("q4_spiderplot"),
-          textOutput("q4_descript")
+          p("This radar chart provides users with a visual way of comparing between two different group types.
+            These groups can vary by age and ethnicity. The groups are compared by percentages of the total population
+            within that group that meet the categories labeled on the corners of the chart.  
+            Mousing over a point will show the exact percentage of people, in their respective group, that meet the category. 
+            Here is what each category means:"),
+          HTML({
+            paste("<strong>Plans of Suicide</strong>
+                  <li>Indicates what percentage of the selected group has 
+                  had Recurrent thoughts of death or recurrent suicide ideation</li>",
+                  "<strong>Depression</strong>
+                  <li>indicates having five or more symptoms of Major Depressive Episodes</li>",
+                  "<strong>Taking Prescription Medicine</strong>
+                  <li>is currently taking medicine prescribed to treat depression</li>",
+                  "<strong>Recieving Professional Treatment</strong>
+                  <li>is currently recieving professional treatment</li>",
+                  "<strong>Poverty Level</strong>
+                  <li>household income meets the federal level of poverty</li>")
+          })
+          
         )
       )
     )
