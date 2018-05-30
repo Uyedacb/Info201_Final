022a4cca28c93427b576e57a0206ee1ac48cf05c
Annie C <anniejchan@gmail.com>
2018-05-30 15:59:44 -0700

    Finalize Q3

--- Analysis.R
+++ Analysis.R
@@ -6,47 +6,59 @@ library(reshape2)
 library(tibble)
 
 # Load filtered data
-filtered_data <- read.csv("Filtered2.csv")
+filtered_data_q3 <- read.csv("Filtered2.csv")
 # View(filtered_data)
 
 # Select necessary columns
-q3_filter <- select(filtered_data, X, QUESTID2, filedate, irsex, suicthnk, suicplan, suictry)
+q3_filter <- select(filtered_data_q3, X, QUESTID2, filedate, irsex, suicthnk, suicplan, suictry)
 
 # Filter behaviors
-both_genders <- paste("\"Male\" | Gender == \"Female\"")
-selected <- select(q3_filter, irsex, suicthnk, suicplan, suictry)
+both_genders_q3 <- paste("\"Male\" | Gender == \"Female\"")
+selected_q3 <- select(q3_filter, irsex, suicthnk, suicplan, suictry)
+
 
 # Create new data frame for frequency counts for bar graph
-selected$irsex[selected$irsex == 1] <- "Male"
-selected$irsex[selected$irsex == 2] <- "Female"
-selected$suicthnk[selected$suicthnk == 1] <- "Seriously thought of"
-selected$suicplan[selected$suicplan == 1] <- "Planned"
-selected$suictry[selected$suictry == 1] <- "Attempted"
-selected_stacked <- melt(selected, id.vars=1)
-yes_only <- c("Seriously thought of", "Planned", "Attempted")
-selected_stacked_2 <- filter(selected_stacked, value == yes_only)
-behaviors_frequencies <- select(selected_stacked_2, irsex, value)
-colnames(behaviors_frequencies) <- c("Gender", "Behavior")
+selected_q3$irsex[selected_q3$irsex == 1] <- "Male"
+selected_q3$irsex[selected_q3$irsex == 2] <- "Female"
+selected_q3$suicthnk[selected_q3$suicthnk == 1] <- "Seriously thought of"
+selected_q3$suicplan[selected_q3$suicplan == 1] <- "Planned"
+selected_q3$suictry[selected_q3$suictry == 1] <- "Attempted"
+selected_stacked_q3 <- melt(selected_q3, id.vars=1)
+yes_only_q3 <- c("Seriously thought of", "Planned", "Attempted")
+selected_stacked_2_q3 <- filter(selected_stacked_q3, value == yes_only_q3)
+behaviors_frequencies_q3 <- select(selected_stacked_2_q3, irsex, value)
+colnames(behaviors_frequencies_q3) <- c("Gender", "Behavior")
+
 
 # Create new data frame for frequency table
-male_filter <- filter(behaviors_frequencies, Gender == "Male")
-male_suicthnk <- nrow(filter(male_filter, Behavior == "Seriously thought of")) 
+male_filter_q3 <- filter(behaviors_frequencies_q3, Gender == "Male")
+male_suicthnk_q3 <- nrow(filter(male_filter_q3, Behavior == "Seriously thought of")) 
 # 329 males seriously thought
-male_suicplan <- nrow(filter(male_filter, Behavior == "Planned")) # 109 males planned
-male_suictry <- nrow(filter(male_filter, Behavior == "Attempted")) # 31 males attempted
-female_filter <- filter(behaviors_frequencies, Gender == "Female")
-female_suicthnk <- nrow(filter(female_filter, Behavior == "Seriously thought of"))
+male_suicplan_q3 <- nrow(filter(male_filter_q3, Behavior == "Planned")) # 109 males planned
+male_suictry_q3 <- nrow(filter(male_filter_q3, Behavior == "Attempted")) # 31 males attempted
+female_filter_q3 <- filter(behaviors_frequencies_q3, Gender == "Female")
+female_suicthnk_q3 <- nrow(filter(female_filter_q3, Behavior == "Seriously thought of"))
 # 442 females seriously thought
-female_suicplan <- nrow(filter(female_filter, Behavior == "Planned")) # 153 females planned
-female_suictry <- nrow(filter(female_filter, Behavior == "Attempted")) # 92 females attempted
-male_comb <- c(male_suicthnk, male_suicplan, male_suictry)
-female_comb <- c(female_suicthnk, female_suicplan, female_suictry)
-suicthnk <- nrow(filter(behaviors_frequencies, Behavior == "Seriously thought of"))
-suicplan <- nrow(filter(behaviors_frequencies, Behavior == "Planned"))
-suictry <- nrow(filter(behaviors_frequencies, Behavior == "Attempted"))
-both_comb <- c(suicthnk, suicplan, suictry)
-genders_comb <- rbind(male_comb, female_comb, both_comb)
-genders_comb <- as.data.frame(genders_comb)
-colnames(genders_comb) <- c("Seriously thought", "Planned", "Attempted")
-row.names(genders_comb) <- c("Male", "Female", "Both")
-genders_comb <- rownames_to_column(genders_comb, var="Gender")
+female_suicplan_q3 <- nrow(filter(female_filter_q3, Behavior == "Planned")) # 153 females planned
+female_suictry_q3 <- nrow(filter(female_filter_q3, Behavior == "Attempted")) # 92 females attempted
+male_comb_q3 <- c(male_suicthnk_q3, male_suicplan_q3, male_suictry_q3)
+female_comb_q3 <- c(female_suicthnk_q3, female_suicplan_q3, female_suictry_q3)
+suicthnk_q3 <- nrow(filter(behaviors_frequencies_q3, Behavior == "Seriously thought of"))
+suicplan_q3 <- nrow(filter(behaviors_frequencies_q3, Behavior == "Planned"))
+suictry_q3 <- nrow(filter(behaviors_frequencies_q3, Behavior == "Attempted"))
+both_comb_q3 <- c(suicthnk_q3, suicplan_q3, suictry_q3)
+genders_comb_q3 <- rbind(male_comb_q3, female_comb_q3, both_comb_q3)
+genders_comb_q3 <- as.data.frame(genders_comb_q3)
+colnames(genders_comb_q3) <- c("Seriously thought", "Planned", "Attempted")
+row.names(genders_comb_q3) <- c("Male", "Female", "Both")
+genders_comb_q3 <- rownames_to_column(genders_comb_q3, var="Gender")
+
+# Filter behaviors
+both_genders_q3 <- paste("\"Male\" | Gender == \"Female\"")
+selected_q3 <- select(q3_filter, irsex, suicthnk, suicplan, suictry)
+total_responses_q3 <- nrow(behaviors_frequencies_q3)
+total_males_q3 <- nrow(filter(behaviors_frequencies_q3, Gender == "Male")) #469
+total_females_q3 <- nrow(filter(behaviors_frequencies_q3, Gender == "Female")) # 687
+total_responses_q3 <- nrow(behaviors_frequencies_q3)
+total_males_q3 <- nrow(filter(behaviors_frequencies_q3, Gender == "Male")) #469
+total_females_q3 <- nrow(filter(behaviors_frequencies_q3, Gender == "Female")) # 687
--- app.R
+++ app.R
@@ -7,22 +7,24 @@ library(shiny)
 source("./Analysis.R")
 
 # Define UI
-my_ui <- fluidPage(
+q3_ui <- fluidPage(
   h5("Frequencies"),
   h6("Had thoughts, planned, or attempted suicide in the past 12 months."),
   sidebarLayout( # Create layout with 2 panels
     sidebarPanel( # Specify content for sidebar panel
       # Create widget
       selectInput("select_gender", "Select a Gender:",
-                  c(list("Both" = "Both"),
+                  c(list("Both" = "Both Genders"),
                     "Male" = "Male",
                     "Female" = "Female"))
     ),
     mainPanel( # Specify content for main panel
       tabsetPanel(type = "tabs",
-                  tabPanel("Visual", plotOutput("q3_plot")),
+                  tabPanel("Visual", plotOutput("q3_plot"),
+                  verbatimTextOutput("description_1_q3")),
                   tabPanel("Table", tableOutput("q3_table")),
-                  tabPanel("Pie Chart", plotOutput("q3_pie"))
+                  tabPanel("Pie Chart", plotOutput("q3_pie"),
+                  verbatimTextOutput("description_2_q3"))
     )
   )
   )
@@ -30,9 +32,9 @@ my_ui <- fluidPage(
 
 # Define server
 q3_server <- function(input, output) {
-  reactive_plot <- reactive({
+  reactive_plot_q3 <- reactive({
     if (input$select_gender != "Male" | input$select_gender != "Female") {
-    plot_both <- ggplot(data = behaviors_frequencies, aes(x = Behavior)) +
+    plot_both_q3 <- ggplot(data = behaviors_frequencies_q3, aes(x = Behavior)) +
       geom_bar(stat = "count", fill = "steelblue") +
       ggtitle("Frequencies of Behaviors in the Past 12 Months") +
       labs(x = "Behavior", y = "Number of People") +
@@ -40,7 +42,7 @@ q3_server <- function(input, output) {
                  position = "identity", color = "white")
     }
     if (input$select_gender == "Male" | input$select_gender == "Female") {
-    filtered_freq <- filter(behaviors_frequencies, Gender == input$select_gender)
+    filtered_freq_q3 <- filter(behaviors_frequencies_q3, Gender == input$select_gender)
     plot_both <- ggplot(data = filtered_freq, aes(x = Behavior)) +
       geom_bar(stat = "count", fill = "steelblue") +
       ggtitle("Frequencies of Behaviors in the Past 12 Months") +
@@ -48,41 +50,60 @@ q3_server <- function(input, output) {
       stat_count(aes(label = ..count..), vjust = 1, geom = "text", 
                  position = "identity", color = "white")
     }
-  return(plot_both)
+  return(plot_both_q3)
   })
   output$q3_plot <- renderPlot(
-    reactive_plot()
+    reactive_plot_q3()
   )
-  reactive_table <- reactive({
+  reactive_table_q3 <- reactive({
     if (input$select_gender != "Male" | input$select_gender != "Female") {
-    filtered_table <- genders_comb
+    filtered_table_q3 <- genders_comb_q3
     }
     if (input$select_gender == "Male" | input$select_gender == "Female") {
     filtered_table <- filter(genders_comb, Gender == input$select_gender)
     }
-    return(filtered_table)
+    return(filtered_table_q3)
   })
   output$q3_table <- renderTable(
-    reactive_table()
+    reactive_table_q3()
   )
-  reactive_pie <- reactive({
+  reactive_pie_q3 <- reactive({
     if (input$select_gender != "Male" | input$select_gender != "Female") {
-      filtered_pie_all <- ggplot(behaviors_frequencies, aes(x = factor(1), fill = factor(Behavior))) +
+      filtered_pie_all_q3 <- ggplot(behaviors_frequencies_q3, aes(x = factor(1), fill = factor(Behavior))) +
         geom_bar(width = 1)
-      filtered_pie <- filtered_pie_all + coord_polar(theta = "y")
+      filtered_pie_q3 <- filtered_pie_all_q3 + coord_polar(theta = "y") +
+        ggtitle("Frequencies of Behaviors in the Past 12 Months") +
+        labs(x = "Behavior", y = "Number of People")
     }
     if (input$select_gender == "Male" | input$select_gender == "Female") {
-      filtered_beh_freq <- filter(behaviors_frequencies, Gender == input$select_gender)
-      filtered_pie_freq <- ggplot(filtered_beh_freq, aes(x = factor(1), fill = factor(Behavior))) +
+      q3_filtered_beh_freq <- filter(behaviors_frequencies_q3, Gender == input$select_gender)
+      filtered_pie_freq <- ggplot(q3_filtered_beh_freq, aes(x = factor(1), fill = factor(Behavior))) +
         geom_bar(width = 1)
-      filtered_pie <- filtered_pie_freq + coord_polar(theta = "y")
+      filtered_pie_q3 <- filtered_pie_freq + coord_polar(theta = "y") +
+        ggtitle("Frequencies of Behaviors in the Past 12 Months") +
+        labs(x = "Behavior", y = "Number of People")
     }
-    return(filtered_pie)
+    return(filtered_pie_q3)
   })
   output$q3_pie <- renderPlot(
-    reactive_pie()
+    reactive_pie_q3()
   )
+  output$description_1_q3 <- renderText({
+    paste0("This bar chart shows the frequencies of behaviors for ", input$select_gender, ".",
+           " There were a total of ", total_responses_q3, " responses and each responder were able to select more than one choice.",
+           "\nThere were a total of ", total_males_q3, " males and a total of ", total_females_q3, " females.",
+           "\nBoth males and females had over half of its respondents select that they have seriously thought of suicide.",
+           "\nBased off of both charts, we know that more than half of both males and females have seriously thought of suicide while less than half of both males and females have attempted suicide.")
+  })
+  output$description_2_q3 <- renderText({
+    paste0("This pie chart shows the proportions of behaviors for ", input$select_gender, ".",
+           " This information is consistent with the bar chart and table. ",
+           "\nThere were a total of ", total_responses_q3, " responses and each responder were able to select more than one choice.",
+           "\nThere were a total of ", total_males_q3, " males and a total of", total_females_q3, ".", "females.",
+           "\nThis chart allows us to see that at least two-thirds of both males and females have seriously thought of suicide.",
+           "\nThe responses for females are less disproportioned than males, given that nearly one-fourth of females have planned suicide.")
+  })
 }
 
 # Combine UI and Server
-shinyApp(ui = my_ui, server = q3_server)
+shinyApp(ui = q3_ui, server = q3_server)
