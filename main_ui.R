67c95ab4e3d218ccd5b1e46a7e6cd2fc11ba483f
nhuber1 <nhuber1@uw.edu>
2018-05-30 14:43:18 -0700

    updated main_ui to include my plot

--- main_ui.R
+++ main_ui.R
@@ -1,10 +1,25 @@
 # install.packages("rsconnect")
 library(shiny)
 library(rsconnect)
+library(plotly)
 main_ui <- fluidPage(
   tabsetPanel(
     tabPanel(
       "Q1",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons('q1_radio', "Select data for last
+                       30 days or diagonsed depression",
+                       c('Last 30 days',' Diagnosed Depression')),
+          sliderInput('q1_slider',
+                      "How many days have been affected by mental health",
+                      1, 30)
+          
+        ),
+        mainPanel(
+          plotOutput('q1_plot')
+        )
+      )
     ),
     tabPanel(
       "Q2",
